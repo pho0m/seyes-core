2b361f38cb130fdf95722b2fa3e5e3d6218640df
pho0m <kiattiphoom@studiotwist.co>
2023-03-10 00:20:52 +0700

    fix(structure): fixed rtsp and server

--- rtsp/docs/examples/webrtc/main.js
+++ rtsp/docs/examples/webrtc/main.js
@@ -1,52 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-  function startPlay (videoEl, url) {
-    const webrtc = new RTCPeerConnection({
-      iceServers: [{
-        urls: ['stun:stun.l.google.com:19302']
-      }],
-      sdpSemantics: 'unified-plan'
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  function startPlay(videoEl, url) {
+    const webrtc = new RTCPeerConnection({});
     webrtc.ontrack = function (event) {
-      console.log(event.streams.length + ' track is delivered')
-      videoEl.srcObject = event.streams[0]
-      videoEl.play()
-    }
-    webrtc.addTransceiver('video', { direction: 'sendrecv' })
-    webrtc.onnegotiationneeded = async function handleNegotiationNeeded () {
-      const offer = await webrtc.createOffer()
+      console.log(event.streams.length + " track is delivered");
+      videoEl.srcObject = event.streams[0];
+      videoEl.play();
+    };
+    webrtc.addTransceiver("video", { direction: "sendrecv" });
+    webrtc.onnegotiationneeded = async function handleNegotiationNeeded() {
+      const offer = await webrtc.createOffer();
 
-      await webrtc.setLocalDescription(offer)
+      await webrtc.setLocalDescription(offer);
 
       fetch(url, {
-        method: 'POST',
-        body: new URLSearchParams({ data: btoa(webrtc.localDescription.sdp) })
+        method: "POST",
+        body: new URLSearchParams({ data: btoa(webrtc.localDescription.sdp) }),
       })
-        .then(response => response.text())
-        .then(data => {
+        .then((response) => response.text())
+        .then((data) => {
           try {
             webrtc.setRemoteDescription(
-              new RTCSessionDescription({ type: 'answer', sdp: atob(data) })
-            )
+              new RTCSessionDescription({ type: "answer", sdp: atob(data) })
+            );
           } catch (e) {
-            console.warn(e)
+            console.warn(e);
           }
-        })
-    }
+        });
+    };
 
-    const webrtcSendChannel = webrtc.createDataChannel('rtsptowebSendChannel')
+    const webrtcSendChannel = webrtc.createDataChannel("rtsptowebSendChannel");
     webrtcSendChannel.onopen = (event) => {
-      console.log(`${webrtcSendChannel.label} has opened`)
-      webrtcSendChannel.send('ping')
-    }
+      console.log(`${webrtcSendChannel.label} has opened`);
+      webrtcSendChannel.send("ping");
+    };
     webrtcSendChannel.onclose = (_event) => {
-      console.log(`${webrtcSendChannel.label} has closed`)
-      startPlay(videoEl, url)
-    }
-    webrtcSendChannel.onmessage = event => console.log(event.data)
+      console.log(`${webrtcSendChannel.label} has closed`);
+      startPlay(videoEl, url);
+    };
+    webrtcSendChannel.onmessage = (event) => console.log(event.data);
   }
 
-  const videoEl = document.querySelector('#webrtc-video')
-  const webrtcUrl = document.querySelector('#webrtc-url').value
+  const videoEl = document.querySelector("#webrtc-video");
+  const webrtcUrl = document.querySelector("#webrtc-url").value;
 
-  startPlay(videoEl, webrtcUrl)
-})
+  startPlay(videoEl, webrtcUrl);
+});
