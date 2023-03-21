f11009ef8a1f008682b19cd5ddbeffeee0a0e4e7
pho0m <kiattiphoom@studiotwist.co>
2023-03-21 12:08:24 +0700

    feat(crud): users crud for seyes

--- internal/model/room/room.go
+++ internal/model/room/room.go
@@ -8,7 +8,6 @@ import (
 // Room defines the log notification model details
 type Room struct {
 	m.Model
-
 	Label      string        `json:"label"`
 	CamURL     string        `json:"cam_url"`
 	Status     string        `json:"status"`
